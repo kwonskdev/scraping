3b326983bfb1fa604a5eae54cafa580364de09e1
kwonsk <nerochaos1@gmail.com>
2022-08-11 15:25:10 +0900

    feat: Make info dataframe good to see
    - Extract important columns.
    - Make date column by ssdate & szDDate.
    - Make reservation column by szState.
    - Merge info with Stadium meta.
    - Change saving filename logic.
    - Save with encoding utf-8-sig for Korean not to be broken.
                  no production code change
                  no production code change

--- ys_futsal/scraping_ys_futsal.py
+++ ys_futsal/scraping_ys_futsal.py
@@ -1,4 +1,6 @@
+#TODO: pyinstaller 만들기
 from pandas import DataFrame
+import numpy as np
 import requests
 import time
 import datetime
@@ -47,18 +49,24 @@ def get_all_reservation_info(stadium_code: str=None, month: int=None, day: int=N
     if month > current_month + 1:
         raise ValueError(f'{current_month+1}월 까지 정보만 얻을 수 있습니다.')
 
+    if stadium_code is None:
+        stadium_codes = STADIUM.code
+        filename = f'all_{current_year}_{month}'
+    else:
+        stadium_codes = [stadium_code]
+        filename = f'{stadium_code}_{current_year}_{month}'
+
     if day is None:
         days = get_month_calendar(current_year, month)
-        filename = f'{current_year}_{month}_{today}.csv'
+        filename += f'_{today}.csv'
 
     else:
         days = [datetime.date(current_year, month, day)]
-        filename = f'{current_year}_{month}_{day}_{today}'
+        filename += f'_{day}_{today}.csv'
 
-    if stadium_code is None:
-        stadium_codes = STADIUM.code
-    else:
-        stadium_codes = [stadium_code]
+
+    if month is None:
+        month = current_month
 
     info = []
     for stadium_code, date in product(stadium_codes, days):
@@ -73,17 +81,27 @@ def get_all_reservation_info(stadium_code: str=None, month: int=None, day: int=N
                 LOGGER.info(f'{_info} error : {ERROR[_info]}')
         info.extend(_info)
 
+    #TODO: 예약가능일 경우 stadium이 None으로 나오는 데 채울 방안 마련
     info = DataFrame(info)
+    info.loc[:, 'date'] = info['ssdate'].fillna('') + info['szDDate'].fillna('')
+    info.loc[:, 'reservation'] = np.select(
+        [info['szState']=='N',
+         info['szState']=='Z',
+         info['szState']==''],
+        ['예약완료', '입금대기', '예약가능'])
+    info = info[['date', 'strtime', 'szStadium', 'reservation']]
+    info.columns = ['date', 'time', 'stadium_code', 'reservation']
+    info = info.merge(STADIUM, on=['stadium_code'], how='left')
 
-    info.to_csv(filename, index=False)
+    info.to_csv(filename, index=False, encoding='utf-8-sig')
 
     
 LOGGER = set_logger('log.log')
 URL = 'http://www.futsalbase.com/api/reservation/allList?stadium={stadium_code}&date={date}'
 {'A': '1구장', 'B': '2구장', 'C': '3구장', 'D': '7구장', 'E': '6구장', 'H': '4구장', 'I': '5구장'}
 STADIUM = DataFrame({
-    'code': ['A', 'B', 'C', 'H', 'I', 'E', 'D'],
-    'stadium': ['1구장', '2구장', '3구장', '4구장', '5구장', '6구장', '7구장'],
+    'stadium_code': ['A', 'B', 'C', 'H', 'I', 'E', 'D'],
+    'stadium_name': ['1구장', '2구장', '3구장', '4구장', '5구장', '6구장', '7구장'],
     'size': ['40x20', '40x20', '40x20', '34x20', '31x20', '40x19', '40x19']
 })
 ERROR = {
